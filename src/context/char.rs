97605e4b8c90d9222b4f3d53f7c4d873cd4711e6
Nik Deapen <nik@deapen.com>
2024-11-21 05:32:23 -0800

    [core] Fixed a lifetime issue for parsing chars.

--- src/context/char.rs
+++ src/context/char.rs
@@ -8,10 +8,9 @@ impl<'a> ParseContext<'a> {
     /// Parses the punctuation char `c`. (ignores any white-line-comments prefix)
     ///
     /// Returns `Ok(c, after_c)`.
-    pub fn parse_char<E, F>(&self, c: char, error: F) -> crate::Result<Token, E>
+    pub fn parse_char<E>(self, c: char, error: E) -> crate::Result<'a, Token<'a>, E>
     where
         E: Debug,
-        F: Fn() -> E,
     {
         let (_white, after_white) = self.white_line_comments();
         let mut b: [u8; 4] = [0u8; 4];
@@ -19,7 +18,7 @@ impl<'a> ParseContext<'a> {
         if let (Some(c), after_c) = after_white.exact(c) {
             Ok((c, after_c))
         } else {
-            Err(after_white.to_error(error()))
+            Err(after_white.to_error(error))
         }
     }
 }
