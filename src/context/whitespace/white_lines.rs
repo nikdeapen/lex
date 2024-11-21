b49cb201479f62cf12bd0d16a585847c4465c7ac
Nik Deapen <nik@deapen.com>
2024-11-21 04:52:51 -0800

    [core] Added support for parsing whitespace.

---
+++ src/context/whitespace/white_lines.rs
@@ -0,0 +1,17 @@
+use crate::ParseContext;
+
+impl<'a> ParseContext<'a> {
+    //! White Lines
+
+    /// Parses optional non-empty whitespace & line-endings.
+    ///
+    /// Returns `(Some(line_ending), after_line_ending)`.
+    /// Returns `(None, self)` when there is no line-ending.
+    pub fn white_lines(&self) -> (Option<Self>, Self) {
+        unsafe {
+            self.match_prefix_optional_unchecked(|c| {
+                c == b' ' || c == b'\t' || c == b'\r' || c == b'\n'
+            })
+        }
+    }
+}
